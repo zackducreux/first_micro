7ea69166f12dcd574c474e6a0fe14af54a7583d8
zackducreux <zackducreux@gmail.com>
2022-11-29 15:05:37 +0100

    second commit

--- main.py
+++ main.py
@@ -3,5 +3,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
+
 def hello_world():
-    return "Hello World & Zackary Ducreux"
+ prefix_google = """
+ <!-- Google tag (gtag.js) -->
+<script async
+src="https://www.googletagmanager.com/gtag/js?id=UA-250386229-1"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', ' UA-250386229-1');
+</script>
+ """
+ return prefix_google + "Hello World"
