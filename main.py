b113f5b00a6d681f620d502f646b24c2f3e42423
zackducreux <zackducreux@gmail.com>
2022-12-06 15:00:11 +0100

    push

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', methods=["GET"])
+@app.route('/name', methods=["GET"])
 def hello_world():
     prefix_google = """
  <!-- Google tag (gtag.js) -->
@@ -17,3 +17,23 @@ src="https://www.googletagmanager.com/gtag/js?id=UA-250386229-1"></script>
 </script>
  """
     return prefix_google + "Hello World & Zackary Ducreux"
+
+@app.route("/")
+def hello():
+    app.logger.debug("A debug message")
+    app.logger.info("An info message")
+    app.logger.warning("A warning message")
+    app.logger.error("An error message")
+    app.logger.critical("A critical message")
+
+    return "Hello, World, no error for now!"
+
+@app.route("/info")
+def info():
+    app.logger.info("Hello, World!")
+    return "Hello, World! (info)"
+
+@app.route("/warning")
+def warning():
+    app.logger.warning("A warning message.")
+    return "A warning message. (warning)"
