d06f7ec78d5f068def58abf0a30ace54958ae217
zackducreux <zackducreux@gmail.com>
2023-01-04 11:03:40 +0100

    "Commit TP3"

--- main.py
+++ main.py
@@ -24,26 +24,26 @@ src="https://www.googletagmanager.com/gtag/js?id=UA-250386229-1"></script>
  """
     return prefix_google + "Hello World & Zackary Ducreux"
 
-@app.route("/simple_chart")
-def chart():
-    # # execute the TrendReq method by passing the host language (hl) and timezone (tz) parameters
-    # pytrends = TrendReq(hl='en-US', tz=360)
-    #
-    # # build list of keywords
-    # kw_list = ["ai", "chicken", "space"]
-    #
-    # # build the payload
-    # pytrends.build_payload(kw_list, timeframe='2015-01-01 2015-03-31', geo='US')
-    #
-    # # store interest over time information in df
-    # df = pytrends.interest_over_time()
-    #
-    # # display the top 20 rows in dataframe
-    # print(df)
-    legend = 'Monthly Data'
-    labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
-    values = [10, 9, 8, 7, 6, 4, 7, 8]
-    return render_template('chart.html', values=values, labels=labels, legend=legend)
+# @app.route("/simple_chart")
+# def chart():
+#     # # execute the TrendReq method by passing the host language (hl) and timezone (tz) parameters
+#     # pytrends = TrendReq(hl='en-US', tz=360)
+#     #
+#     # # build list of keywords
+#     # kw_list = ["ai", "chicken", "space"]
+#     #
+#     # # build the payload
+#     # pytrends.build_payload(kw_list, timeframe='2015-01-01 2015-03-31', geo='US')
+#     #
+#     # # store interest over time information in df
+#     # df = pytrends.interest_over_time()
+#     #
+#     # # display the top 20 rows in dataframe
+#     # print(df)
+#     legend = 'Monthly Data'
+#     labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
+#     values = [10, 9, 8, 7, 6, 4, 7, 8]
+#     return render_template('chart.html', values=values, labels=labels, legend=legend)
 
 @app.route("/")
 def hello():
