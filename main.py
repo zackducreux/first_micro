36bc8ad97414d1c299665534c7a8bd885d8ea702
zackducreux <zackducreux@gmail.com>
2023-01-04 11:01:00 +0100

    "Commit TP3"

--- main.py
+++ main.py
@@ -1,28 +1,13 @@
-from flask import Flask
+# import the TrendReq method from the pytrends request module
+from pytrends.request import TrendReq
+from flask import Flask, render_template
 import sys
 
 app = Flask(__name__)
 
 
-# import the TrendReq method from the pytrends request module
-from pytrends.request import TrendReq
-import pandas as pd
-import flask
 
-# execute the TrendReq method by passing the host language (hl) and timezone (tz) parameters
-pytrends = TrendReq(hl='en-US', tz=360)
 
-# build list of keywords
-kw_list = ["ai", "chicken", "space"]
-
-# build the payload
-pytrends.build_payload(kw_list, timeframe='2015-01-01 2015-03-31', geo='US')
-
-# store interest over time information in df
-df = pytrends.interest_over_time()
-
-# display the top 20 rows in dataframe
-print(df)
 
 @app.route('/name', methods=["GET"])
 def hello_world():
@@ -39,6 +24,26 @@ src="https://www.googletagmanager.com/gtag/js?id=UA-250386229-1"></script>
  """
     return prefix_google + "Hello World & Zackary Ducreux"
 
+@app.route("/simple_chart")
+def chart():
+    # # execute the TrendReq method by passing the host language (hl) and timezone (tz) parameters
+    # pytrends = TrendReq(hl='en-US', tz=360)
+    #
+    # # build list of keywords
+    # kw_list = ["ai", "chicken", "space"]
+    #
+    # # build the payload
+    # pytrends.build_payload(kw_list, timeframe='2015-01-01 2015-03-31', geo='US')
+    #
+    # # store interest over time information in df
+    # df = pytrends.interest_over_time()
+    #
+    # # display the top 20 rows in dataframe
+    # print(df)
+    legend = 'Monthly Data'
+    labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
+    values = [10, 9, 8, 7, 6, 4, 7, 8]
+    return render_template('chart.html', values=values, labels=labels, legend=legend)
 
 @app.route("/")
 def hello():
@@ -52,15 +57,16 @@ def hello():
     return "Hello, World, no error for now!"
 
 
+
 @app.route("/info")
 def info():
     app.logger.info(f"Hello, World! {df}")
-    return f"Hello, World! {df}"
+    return f"Hello, World! \n {df}"
 
 
 @app.route('/Logger', methods=["GET"])
 def logger():
-    page="""
+    page = """
     <script> console.log('aie')</script>
     """
-    return 'console'+page
+    return 'console' + page
